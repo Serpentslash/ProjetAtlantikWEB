99079edc8c0a55edb718e93b7a42f81212a50449
Serpentslash <22serpentslash22@gmail.com>
2023-05-12 17:04:40 +0200

    2 commit

--- app/Controllers/Visiteur.php
+++ app/Controllers/Visiteur.php
@@ -79,4 +79,74 @@ class Visiteur extends BaseController{
         session()->destroy();
         return redirect()->to('accueil');
     }
+
+    public function ajouterProduit()
+    {
+        $data['TitreDeLaPage'] = 'S\'inscrire';
+
+        if (!$this->request->is('post')) {
+            return view('Templates/vue_Header', $data)
+            . view('Visiteur/vue_Inscrire')
+            . view('Templates/vue_Footer');
+        }
+
+        /* SI FORMULAIRE NON POSTE, LE CODE QUI SUIT N'EST PAS EXECUTE */
+
+        /* VALIDATION DU FORMULAIRE */
+
+        $reglesValidation = [
+            'txtReference' => 'required|alpha_numeric|exact_length[3]',
+            // obligatoire, 3 caractères, exactement
+
+            'txtLibelle' => 'required|string|max_length[30]',
+
+            // obligatoire, chaîne de carac. <= 30 carac.
+
+            'txtPrixHT' => 'permit_empty|numeric',
+
+            // vide ou numérique
+
+            'txtQuantiteEnStock' => 'permit_empty|integer',
+
+            // vide ou integer
+
+            'txtNomFichierImage' => 'permit_empty|string|max_length[20]',
+
+            // vide ou chaîne <= 20 carac.
+
+        ];
+
+        if (!$this->validate($reglesValidation)) {
+            /* formulaire non validé, on renvoie le formulaire */
+            $data['TitreDeLaPage'] = "Saisie incorrecte";
+
+            return view('Templates/vue_Header', $data)
+            . view('Visiteur/vue_Inscrire')
+            . view('Templates/vue_Footer');
+        }
+
+        /* SI FORMULAIRE NON VALIDE, LE CODE QUI SUIT N'EST PAS EXECUTE */
+
+        /* INSERTION PRODUIT SAISI DANS BDD */
+
+        $donneesAInserer = array(
+            'reference' => $this->request->getPost('txtReference'),
+            'libelle' => $this->request->getPost('txtLibelle'),
+            'prixht' => $this->request->getPost('txtPrixHT'),
+            'quantiteenstock' => $this->request->getPost('txtQuantiteEnStock'),
+            'image' => $this->request->getPost('txtNomFichierImage'),
+        ); // reference, libelle, prixht, quantiteenstock, image : champs de la table 'produit'
+
+        $modClient = new ModeleClient(); //instanciation du modèle
+        $donnees['produitAjoute'] = $modClient->insert($donneesAInserer, false);
+
+        // provoque insert into sur la table mappée (produit, ici), retourne 1 (true) si ajout OK
+
+        return view('Templates/Header')
+
+            .view('Administrateur/vue_RapportAjouterProduit', $donnees)
+
+            .view('Templates/Footer');
+
+    } // ajouterProduit
 }
--- app/Models/ModeleClient.php
+++ app/Models/ModeleClient.php
@@ -7,17 +7,6 @@ class ModeleClient extends Model
     protected $table = 'client';
     protected $primaryKey = 'noclient';
     protected $allowedFields = ['NOM', 'PRENOM', 'ADRESSE','CODEPOSTAL', 'VILLE', 'TELEPHONEFIXE', 'TELEPHONEMOBILE', 'MEL', 'MOTDEPASSE'];
-   
-    public function seConnecter()
-    {
-        $db=\Config\Database::connect();
-        $builder = $db->table('client');
-        $builder->insert('NOM', 'PRENOM', 'ADRESSE','CODEPOSTAL', 'VILLE', 'TELEPHONEFIXE', 'TELEPHONEMOBILE', 'MEL', 'MOTDEPASSE');
-
-        return $builder->get(); /* Pour générer le tableau automatiquement, sinon
-        return $builder->get()->getResult();  (voir vue associée)
-        */
-    }
 
     public function retournerClient($MEL, $password)
     {
