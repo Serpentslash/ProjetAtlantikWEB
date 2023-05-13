4708d615a95f872897e5198d069bf2d827434105
Serpentslash <22serpentslash22@gmail.com>
2023-05-13 16:25:45 +0200

    Regles de Validation

--- app/Controllers/Visiteur.php
+++ app/Controllers/Visiteur.php
@@ -13,13 +13,6 @@ class Visiteur extends BaseController{
             . view('Templates/vue_Footer');
     }
 
-    public function inscrire(){
-        $data['TitreDeLaPage'] = 'S\'inscrire';
-        return view('Templates/vue_Header', $data)
-            . view('Visiteur/vue_Inscrire')
-            . view('Templates/vue_Footer');
-    }
-
     public function connecter()
     {
         helper(['form']);
@@ -80,7 +73,7 @@ class Visiteur extends BaseController{
         return redirect()->to('accueil');
     }
 
-    public function ajouterProduit()
+    public function inscrire()
     {
         $data['TitreDeLaPage'] = 'S\'inscrire';
 
@@ -95,30 +88,87 @@ class Visiteur extends BaseController{
         /* VALIDATION DU FORMULAIRE */
 
         $reglesValidation = [
-            'txtReference' => 'required|alpha_numeric|exact_length[3]',
-            // obligatoire, 3 caractères, exactement
-
-            'txtLibelle' => 'required|string|max_length[30]',
-
-            // obligatoire, chaîne de carac. <= 30 carac.
-
-            'txtPrixHT' => 'permit_empty|numeric',
-
-            // vide ou numérique
-
-            'txtQuantiteEnStock' => 'permit_empty|integer',
-
-            // vide ou integer
-
-            'txtNomFichierImage' => 'permit_empty|string|max_length[20]',
-
-            // vide ou chaîne <= 20 carac.
-
+            'NOM' => [
+                'label' => 'Nom',
+                'rules' => 'required|string|max_length[50]',
+                'errors' => [
+                    'required' => 'Le champ {field} est obligatoire.',
+                    'string' => 'Le champ {field} doit être une chaîne de caractères.',
+                    'max_length' => 'Le champ {field} ne doit pas dépasser {param} caractères.'
+                ]
+            ],
+            'PRENOM' => [
+                'label' => 'Prénom',
+                'rules' => 'required|string|max_length[50]',
+                'errors' => [
+                    'required' => 'Le champ {field} est obligatoire.',
+                    'string' => 'Le champ {field} doit être une chaîne de caractères.',
+                    'max_length' => 'Le champ {field} ne doit pas dépasser {param} caractères.'
+                ]
+            ],
+            'ADRESSE' => [
+                'label' => 'Adresse',
+                'rules' => 'required|string|max_length[255]',
+                'errors' => [
+                    'required' => 'Le champ {field} est obligatoire.',
+                    'string' => 'Le champ {field} doit être une chaîne de caractères.',
+                    'max_length' => 'Le champ {field} ne doit pas dépasser {param} caractères.'
+                ]
+            ],
+            'CODEPOSTAL' => [
+                'label' => 'Code Postal',
+                'rules' => 'required|exact_length[5]|integer',
+                'errors' => [
+                    'required' => 'Le champ {field} est obligatoire.',
+                    'exact_length' => 'Le champ {field} doit contenir exactement {param} chiffres.',
+                    'integer' => 'Le champ {field} doit être un entier.'
+                ]
+            ],
+            'VILLE' => [
+                'label' => 'Ville',
+                'rules' => 'required|string|max_length[50]',
+                'errors' => [
+                    'required' => 'Le champ {field} est obligatoire.',
+                    'string' => 'Le champ {field} doit être une chaîne de caractères.',
+                    'max_length' => 'Le champ {field} ne doit pas dépasser {param} caractères.'
+                ]
+            ],
+            'TELEPHONEFIXE' => [
+                'label' => 'Téléphone Fixe',
+                'rules' => 'permit_empty|regex_match[/(0|\+33)[1-9]( *[0-9]{2}){4}/]',
+                'errors' => [
+                    'regex_match' => 'Le champ {field} doit être un numéro de téléphone fixe valide en France (exemple : 01 23 45 67 89).'
+                ]
+            ],
+            'TELEPHONEMOBILE' => [
+                'label' => 'Téléphone Mobile',
+                'rules' => 'required|regex_match[/(0|\+33)[67]( *[0-9]{2}){4}/]',
+                'errors' => [
+                    'required' => 'Le champ {field} est obligatoire.',
+                    'regex_match' => 'Le champ {field} doit être un numéro de téléphone mobile valide en France (exemple : 06 12345678).'
+                ]
+            ],
+            'MEL' => [
+            'label' => 'Adresse email',
+            'rules' => 'required|valid_email',
+            'errors' => [
+            'required' => 'Le champ {field} est obligatoire.',
+            'valid_email' => 'Le champ {field} doit être une adresse email valide.'
+                ]
+            ],
+            'MOTDEPASSE' => [
+            'label' => 'Mot de passe',
+            'rules' => 'required|min_length[8]',
+            'errors' => [
+            'required' => 'Le champ {field} est obligatoire.',
+            'min_length' => 'Le champ {field} doit contenir au moins {param} caractères.'
+                ]
+            ]
         ];
 
         if (!$this->validate($reglesValidation)) {
             /* formulaire non validé, on renvoie le formulaire */
-            $data['TitreDeLaPage'] = "Saisie incorrecte";
+            $data['TitreDeLaPage'] = "S'inscrire";
 
             return view('Templates/vue_Header', $data)
             . view('Visiteur/vue_Inscrire')
